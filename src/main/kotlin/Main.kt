867f04dbb46d978815e2c1caf0a018d761526023
Glagolev Mikhail <mihpihnaty@yandex.ru>
2020-07-13 16:59:17 +0300

    :bug: fix args order (#5)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import com.elbekD.bot.Bot
 fun main(args: Array<String>) {
     val token = System.getenv("TELEGRAM_TOKEN") ?: throw RuntimeException("Unable to get system variable for token")
     val userName = "PullPartyBot"
-    val bot = Bot.createPolling(token, userName)
+    val bot = Bot.createPolling(userName, token)
 
     bot.onCommand("/start") { msg, _ ->
         bot.sendMessage(msg.chat.id, "Hello World!")
