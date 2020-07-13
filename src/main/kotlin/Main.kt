75d2afb9ef272850fcb02b40da9d4f8de60cdb2b
ShuffleZZZ <mihpihnaty@yandex.ru>
2020-07-13 16:19:11 +0300

    :art: change system variable name, add exception on fail, explicitly declare bot name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 import com.elbekD.bot.Bot
 
 fun main(args: Array<String>) {
-    val token = System.getenv("token") ?: "undefined"
-    val userName = "<USER_NAME>"
+    val token = System.getenv("TELEGRAM_TOKEN") ?: throw RuntimeException("Unable to get system variable for token")
+    val userName = "PullPartyBot"
     val bot = Bot.createPolling(token, userName)
 
     bot.onCommand("/start") { msg, _ ->
