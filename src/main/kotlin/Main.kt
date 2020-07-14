8027c7476451e67c8600d8aeb7c9b9bad893449a
ShuffleZZZ <mihpihnaty@yandex.ru>
2020-07-14 13:13:36 +0300

    :sparkles: start bot commands handling

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -22,5 +22,42 @@ fun main() {
         bot.sendMessage(msg.chat.id, "Hello World!")
     }
 
+    bot.onCommand("/help") { msg, _ ->
+        bot.sendMessage(msg.chat.id, HELP_MSG);
+    }
+
+    bot.onCommand("/create") { msg, list ->
+        val parsedList = list?.split(' ')?.map { it.trim() }
+        val chatId = msg.chat.id
+
+        if (parsedList.isNullOrEmpty() || parsedList.size == 1) {
+            bot.sendMessage(chatId, onCreateFail)
+        } else {
+            val partyName = parsedList[0]
+            val users = parsedList.drop(1)
+
+            bot.sendMessage(chatId, "Party $partyName successfully created!")
+        }
+    }
+
     bot.start()
 }
+
+const val HELP_MSG =
+    """
+Available commands:
+
+    /start    - awake the bot
+    /help     - show this usage guide
+    /create <name @user1 @user2 ...>    - create new party with given name and users
+    """
+
+const val onCreateFail =
+    """
+No people - no party 😔
+
+Follow the create command with the party name and members of new group
+At least name and single user should be provided
+
+Type /help for more information
+    """
