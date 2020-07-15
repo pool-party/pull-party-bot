a733848d8c11049392a0cd4b9b565335e62728df
Vitaliy <epifantsev.vitaliy@gmail.com>
2020-07-15 12:53:11 +0300

    :rocket: add webhooks (#19)
    * poebalsya nochui znatno, konchil na lico webhuiem monkaShake
    
    * nu cho narod pognai nahui
    
    * nu slash lishniu happens
    
    * test
    
    * another one
    
    * last attempt i ya spat'
    
    * dsasdasdaswith '#' will be ignored, and an empty message aborts the commit.
    
    * ss
    
    * upd urls
    
    * sd
    
    * webhook info
    
    * with '#' will be ignored, and an empty message aborts the commit.
    
    * last
    
    * kekW
    
    * st"x"
    
    * sss
    
    * ss
    
    * WELCOME TO THE WORLD OF WEBHOOKS WIDEPEEPOHAPPY
    
    * :art: apply editorconfig
    
    Co-authored-by: vileme <wickdtarazed@gmail.com>
    Co-authored-by: ShuffleZZZ <mihpihnaty@yandex.ru>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,19 @@
 import com.elbekD.bot.Bot
+import com.elbekD.bot.server
+import com.elbekD.bot.util.AllowedUpdate
 
 fun main() {
+    val app = "https://pullpartybot.herokuapp.com"
     val token = System.getenv("TELEGRAM_TOKEN") ?: throw RuntimeException("Unable to get system variable for token")
     val userName = "PullPartyBot"
-    val bot = Bot.createPolling(userName, token)
+    val bot = Bot.createWebhook(userName,token) {
+        url = "${app}/${token}"
+        allowedUpdates = listOf(AllowedUpdate.Message)
+        server {
+            host = "0.0.0.0"
+            port = System.getenv("PORT").toInt()
+        }
+    }
 
     bot.onCommand("/start") { msg, _ ->
         bot.sendMessage(msg.chat.id, "Hello World!")
