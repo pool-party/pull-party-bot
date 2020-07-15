823c8770d4c4258e8175c5d933f351abbc2ab5c5
DubKoldun <vitaliy.epifantsev@yandex.ru>
2020-07-15 14:54:02 +0300

    :twisted_rightwards_arrows: added webhook integration

--- src/main/kotlin/com/github/pool_party/pull_party_bot/Main.kt
+++ src/main/kotlin/com/github/pool_party/pull_party_bot/Main.kt
@@ -3,11 +3,25 @@ package com.github.pool_party.pull_party_bot
 import com.elbekD.bot.Bot
 import com.github.pool_party.pull_party_bot.commands.initializePingCommands
 
+const val APP_URL = "https://pullpartybot.herokuapp.com"
 const val USER_NAME = "PullPartyBot"
+const val DEFAULT_PORT = 80
 
 fun main() {
     val token = System.getenv("TELEGRAM_TOKEN") ?: throw RuntimeException("Unable to get system variable for token")
-    val bot = Bot.createPolling(USER_NAME, token)
-    initializePingCommands(bot)
+    val bot = Bot.createWebhook(USER_NAME,token) {
+        url = "${APP_URL}/${token}"
+        allowedUpdates = listOf(AllowedUpdate.Message)
+
+        server {
+            host = "0.0.0.0"
+            port = System.getenv("PORT")?.toInt() ?: DEFAULT_PORT
+        }
+    }
+
+    bot.onCommand("/start") { msg, _ ->
+        bot.sendMessage(msg.chat.id, "Hello World!")
+    }
+
     bot.start()
 }
