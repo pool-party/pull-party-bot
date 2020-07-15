a5648b1297d72ce71de141d936f9575405893e4b
vileme <wickdtarazed@gmail.com>
2020-07-15 13:25:42 +0400

    WELCOME TO THE WORLD OF WEBHOOKS WIDEPEEPOHAPPY

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,53 +1,22 @@
 import com.elbekD.bot.Bot
 import com.elbekD.bot.server
-import com.elbekD.bot.types.Message
 import com.elbekD.bot.util.AllowedUpdate
-import io.ktor.application.*
-
-import io.ktor.routing.get
-import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
-import io.ktor.request.receive
-import io.ktor.request.receiveText
-import io.ktor.response.respond
-import io.ktor.response.respondText
-import io.ktor.routing.post
-import io.ktor.server.engine.applicationEngineEnvironment
-import io.ktor.server.engine.sslConnector
 
 fun main() {
-    val ngrok = "https://gankedbymomtestdeploy.herokuapp.com"
+    val app = "https://pullpartybot.herokuapp.com"
     val token = System.getenv("TELEGRAM_TOKEN") ?: throw RuntimeException("Unable to get system variable for token")
     val userName = "PullPartyBot"
     val bot = Bot.createWebhook(userName,token) {
-        url = "${ngrok}/${token}"
+        url = "${app}/${token}"
         allowedUpdates = listOf(AllowedUpdate.Message)
         server {
             host = "0.0.0.0"
             port = System.getenv("PORT").toInt()
         }
     }
-    print(System.getenv("PORT"))
-    print(bot.getWebhookInfo().get())
     bot.onCommand("/start") { msg, _ ->
         bot.sendMessage(msg.chat.id, "Hello World!")
     }
 
-
-//    embeddedServer(Netty,8443) {
-//        routing {
-//            get("/"){
-//                call.respondText("Hi Mark", ContentType.Text.Html)
-//            }
-//            post("/${token}") {
-//                val response = call.receiveText()
-//                print(response)
-//                call.respond(HttpStatusCode.OK)
-//            }
-//        }
-//    }.start()
     bot.start()
 }
