80ac58b4e88e1dbc295a7114cb6a0ac6fdc803ec
Vitaliy <epifantsev.vitaliy@gmail.com>
2020-07-16 16:22:05 +0300

    :rocket: add commands (#21)
    * :sparkles: start bot commands handling
    
    * :construction: continue the handlers
    
    * :construction: update /help, use returns
    
    * :building_construction: improving code architecture
    
    * :ambulance: hotfixing manifest main class
    
    * :ambulance: hotfixing manifest main class
    
    * :construction: add /list, check null in /delete
    
    * :twisted_rightwards_arrows: added webhook integration
    
    * :bug: add necessary imports
    
    * :sparkles: start bot commands handling
    
    * :construction: continue the handlers
    
    * :construction: update /help, use returns
    
    * :building_construction: improving code architecture
    
    * :ambulance: hotfixing manifest main class
    
    * :ambulance: hotfixing manifest main class
    
    * :construction: add /list, check null in /delete
    
    * :twisted_rightwards_arrows: added webhook integration
    
    * :bug: add necessary imports
    
    * :ambulance: revert critical changes
    
    * :ambulance: app_url changed
    
    * :arrow_up: add db dependencies
    
    * :white_check_mark: heroku app test
    
    * :building_construction: add launch optioning
    
    * :rewind: remove bd work
    
    * :pencil: add TODOs
    
    Co-authored-by: ShuffleZZZ <mihpihnaty@yandex.ru>
    Co-authored-by: Simon Naumov <daretoodefy@gmail.com>

---
+++ src/main/kotlin/com/github/pool_party/pull_party_bot/Main.kt
@@ -0,0 +1,32 @@
+package com.github.pool_party.pull_party_bot
+
+import com.elbekD.bot.Bot
+import com.elbekD.bot.server
+import com.elbekD.bot.util.AllowedUpdate
+import com.github.pool_party.pull_party_bot.commands.initializePingCommands
+
+const val APP_URL = "https://somebodyoncetoldmepool.herokuapp.com"
+const val USER_NAME = "PullPartyBot"
+const val DEFAULT_PORT = 80
+
+fun main() {
+    val token = System.getenv("TELEGRAM_TOKEN") ?: throw RuntimeException("Unable to get system variable for token")
+    val isLongpoll = System.getenv("IS_LONGPOLL")?.toBoolean() ?: false
+
+    val bot = if (isLongpoll) {
+        Bot.createPolling(USER_NAME, token)
+    } else {
+        Bot.createWebhook(USER_NAME, token) {
+            url = "${APP_URL}/${token}"
+            allowedUpdates = listOf(AllowedUpdate.Message)
+
+            server {
+                host = "0.0.0.0"
+                port = System.getenv("PORT")?.toInt() ?: DEFAULT_PORT
+            }
+        }
+    }
+
+    initializePingCommands(bot)
+    bot.start()
+}
---
+++ src/main/kotlin/com/github/pool_party/pull_party_bot/commands/Messages.kt
@@ -0,0 +1,55 @@
+package com.github.pool_party.pull_party_bot.commands
+
+val HELP_MSG =
+    """
+    Available commands:
+
+        /start - awake the bot
+        /help  - show this usage guide
+        /list  - show the parties of the chat
+
+        /party  <party-name> - tag the members of existing party
+        /delete <party-name> - forget the party like it had never happened
+
+        /create <party-name users-list> - create new party
+    """.trimIndent()
+
+val ON_CREATE_FAIL =
+    """
+    No people - no party. 😔
+
+    At least name and a single user should be provided
+    Follow the /create command with the party name and members of a new group
+
+    Type /help for more information
+    """.trimIndent()
+
+val ON_PARTY_FAIL =
+    """
+    I could call up all parties, but it doesn't sound like a good idea. 🤪
+
+    Perhaps you forgot to enter the party name
+    Follow the /party command with the name of existing party
+
+    Type /help for more information
+    """.trimIndent()
+
+val ON_PARTY_REQUEST_FAIL =
+    """
+    I am not aware of this party. You didn't invite me? 🤔
+
+    Perhaps you wanted to create the party or misspelled its name
+    Follow the /party command with the name of existing party
+
+    Type /help for more information
+    """.trimIndent()
+
+val ON_DELETE_EMPTY =
+    """
+    I'm not the police, but can stop the party. Which one though? 🚨
+
+    Perhaps you forgot to enter the name of party to remove
+    Follow the /delete command with the name of redundant party
+
+    Type /help for more information
+    """.trimIndent()
