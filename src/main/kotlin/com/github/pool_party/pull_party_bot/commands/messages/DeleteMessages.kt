d21390ad879fa7e50ff50209faf82763cbe0b4f0
ShuffleZZZ <mihpihnaty@yandex.ru>
2020-12-19 21:05:28 +0300

    :recycle: sync messages and handlers packages

---
+++ src/main/kotlin/com/github/pool_party/pull_party_bot/commands/messages/DeleteMessages.kt
@@ -0,0 +1,63 @@
+package com.github.pool_party.pull_party_bot.commands.messages
+
+val ON_CLEAR_SUCCESS =
+    """
+    Life is a party. Chat is dead then. 😭
+
+    All parties are over. Hope you did it only to rock even bigger ones
+    Let's rest just a little and I'll be waiting for your new entries
+    Use /create command to start all over again
+
+    Type `/help create` for more information
+    """.trimIndent()
+
+val ON_DELETE_EMPTY =
+    """
+    I'm not the police, but can stop the party. Which one though? 🚨
+
+    Perhaps you forgot to enter the name of party to remove
+    Follow the /delete command with the name of redundant party
+
+    Type `/help delete` for more information
+    """.trimIndent()
+
+val ON_SENDER_FAIL =
+    """
+    Only the chosen ones can perform these actions. 😎
+
+    I were not able to recognize the permissions you have in this chat
+    Operation aborted, ask group admins if you still want to make it happen
+
+    Type `/help delete` or `/help clear` for more information
+    """.trimIndent()
+
+val ON_PERMISSION_DENY =
+    """
+    Parties are all I have. You can't just do this. 🤬
+
+    Sad enough for me, parties can be lost and forgotten, but only by admins
+    Only group administrators can perform /delete and /clear commands
+    You can pull @admins party to ask them to perform this command
+
+    Type `/help delete` or `/help clear` for more information
+    """.trimIndent()
+
+val ON_ADMINS_PARTY_CHANGE =
+    """
+    I would be careful disturbing these ladies and gentlemen. 🤫
+
+    @admins is a reserved group, you can't create, change or delete it
+    Try to make a new party instead with /create command
+
+    Type `/help` for more information
+    """.trimIndent()
+
+val ON_ADMINS_PARTY_FAIL =
+    """
+    Admins?! What admins?! 🥴
+
+    There is no admins in private chats or channels
+    @admins is a reserved group, you can't change or delete it
+
+    Type `/help` for more information
+    """.trimIndent()
--- src/main/kotlin/com/github/pool_party/pull_party_bot/commands/messages/InformationMessages.kt
+++ src/main/kotlin/com/github/pool_party/pull_party_bot/commands/messages/InformationMessages.kt
@@ -1,5 +1,17 @@
 package com.github.pool_party.pull_party_bot.commands.messages
 
+import com.github.pool_party.pull_party_bot.Configuration
+
+// Comment template:
+// """
+// Interaction message
+// *Blank line*
+// Try to predict possible action led to this mistake and make a suggestion
+// Small command usage guide
+// *Blank line*
+// /help command suggestion
+// """
+
 val INIT_MSG =
     """
     Hey! I'm a PullPartyBot!
@@ -36,4 +48,137 @@ val HELP_MSG =
 
         /rude      <on/off> - switch RUDE(CAPS LOCK) mode
         /feedback <message> - share your ideas and experience with developers
+    """.trimIndent()
+
+val HELP_START =
+    """
+    /start - start the conversation and see welcoming message
+
+    Or simply remember how it all started 😉
+    """.trimIndent()
+
+val HELP_LIST =
+    """
+    /list entry1? entry2?... - show the parties of the chat and their members
+
+    Returns all parties with no arguments given
+    Entry is either user or party-name:
+      - on the given party shows its members
+      - on the given user shows all parties he is part of
+    Doesn't show @admins party
+    """.trimIndent()
+
+val HELP_CLEAR =
+    """
+    /clear - delete all parties of the chat
+
+    Type `/clear delete` for more information
+    """.trimIndent()
+
+val HELP_PARTY =
+    """
+    /party <party-names> - tag the members of the given parties
+
+    Keep in mind that you can simply tag the parties with `@<party-name>` syntax (bot has to be an admin)
+    If you mention multiple parties - their members will be gathered in a single message and will have no repeats
+    """.trimIndent()
+
+val HELP_DELETE =
+    """
+    /delete <party-names> - delete the parties you provided
+
+    Only admins have access to /delete and /clear commands
+    @admins is a reserved party and can't be deleted
+    """.trimIndent()
+
+val HELP_CREATE =
+    """
+    /create <party-name users-list> - create new party with mentioned users
+
+    Users within the party are not repeating
+    Party should consist of at least one user
+    You can enter users with or without `@` symbol
+    @admins is a reserved party and already exists
+    `@`, ${Configuration.PROHIBITED_SYMBOLS.joinToString { "`$it`" }} symbols and trailing `-` are not allowed in the party name
+    """.trimIndent()
+
+val HELP_CHANGE =
+    """
+    /change <party-name users-list> - change an existing party
+
+    A shorter way to delete and create the party again 👍
+    @admins party can't be changed, follows all /create rules
+
+    Type `/help create` for more information
+    """.trimIndent()
+
+val HELP_ADD =
+    """
+    /add <party-name users-list> - add new users to the given party
+
+    You can enter users with or without `@` symbol
+    You can't update @admins party
+    """.trimIndent()
+
+val HELP_REMOVE =
+    """
+    /remove <party-name users-list> - remove given users from the provided party
+
+    Action can't leave the party empty
+    Action can't leave the party with a single user if name of the user is equal to the party name
+    You can enter users with or without `@` symbol
+    You can't change @admins party
+    """.trimIndent()
+
+val HELP_RUDE =
+    """
+    /rude <on/off> - switch RUDE(CAPS LOCK) mode
+
+    To enable or disable mode follow the command with either `on` or `off`
+    The mode is turned off by default and never affects error messages
+    """.trimIndent()
+
+val HELP_FEEDBACK =
+    """
+    /feedback <message> - share your ideas and experience with developers
+
+    You might suggest the functionality you want to have, report the bugs or share your experience
+    We are always willing to get better for the comfort of our users!
+    """.trimIndent()
+
+val ON_HELP_ERROR =
+    """
+    The Lord helps those who help themselves 👼
+
+    Expected no arguments or command to explain
+    Follow /help with the unclear command or leave empty for general guide
+    """.trimIndent()
+
+val ON_LIST_SUCCESS =
+    """
+    The parties I know:
+
+
+    """.trimIndent()
+
+val ON_LIST_EMPTY =
+    """
+    I don't know any parties in this chat yet 😢
+    """.trimIndent()
+
+val ON_ARGUMENT_LIST_SUCCESS =
+    """
+    Matched parties:
+
+
+    """.trimIndent()
+
+val ON_ARGUMENT_LIST_EMPTY =
+    """
+    I can't find who you are looking for 📭
+
+    Perhaps you misspelled the name of party or user
+    Follow the /list command with correct users and party names
+
+    Type `/help list` for more information
     """.trimIndent()
