50c7cae1fd03f40ed9124f128f163a3217a8bf06
ShuffleZZZ <mihpihnaty@yandex.ru>
2020-12-21 16:58:51 +0300

    update pull tests, start rude tests

--- src/test/kotlin/com/github/pool_party/pull_party_bot/commands/handlers/PullPartyCommandsTest.kt
+++ src/test/kotlin/com/github/pool_party/pull_party_bot/commands/handlers/PullPartyCommandsTest.kt
@@ -5,8 +5,6 @@ import com.elbekD.bot.types.Chat
 import com.elbekD.bot.types.Message
 import com.github.pool_party.pull_party_bot.commands.messages.ON_ADMINS_PARTY_FAIL
 import com.github.pool_party.pull_party_bot.commands.messages.ON_PARTY_EMPTY
-import com.github.pool_party.pull_party_bot.commands.messages.ON_PARTY_REQUEST_FAIL
-import com.github.pool_party.pull_party_bot.database.dao.ChatDao
 import com.github.pool_party.pull_party_bot.database.dao.PartyDao
 import io.mockk.MockKAnnotations
 import io.mockk.every
@@ -24,23 +22,22 @@ internal class PullPartyCommandsTest {
     private lateinit var bot: Bot
 
     @MockK
-    private lateinit var chatDao: ChatDao
-
-    @MockK
     private lateinit var partyDao: PartyDao
 
     private lateinit var party: PartyCommand
     private lateinit var action: suspend (Message, String?) -> Unit
 
-    private var chat =
+    private val groupChat =
         Chat(1, "group", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null)
 
-    private var message = Message(
+    private val privateChat = groupChat.copy(id = 2, type = "private")
+
+    private val groupMessage = Message(
         1,
         null,
         null,
         1,
-        chat,
+        groupChat,
         null,
         null,
         null,
@@ -89,56 +86,103 @@ internal class PullPartyCommandsTest {
         null
     )
 
+    private val privateMessage = groupMessage.copy(message_id = 2, date = 2, chat = privateChat)
+
     @BeforeTest
     fun setupMock() {
         MockKAnnotations.init(this)
 
-        party = PartyCommand(partyDao)
-
         every { bot.onCommand(any(), any()) } answers { action = secondArg() }
+        every {
+            bot.sendMessage(
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                any()
+            )
+        } returns CompletableFuture()
 
+        party = PartyCommand(partyDao)
         party.onMessage(bot)
-
-        every { bot.sendMessage(any(), any(), any()) } returns CompletableFuture()
     }
 
 
     @Test
     fun emptyPartyCall() {
-        GlobalScope.launch { action(message, "") }
-        GlobalScope.launch { action(message, null) }
-
-        verify(exactly = 2) {
-            bot.sendMessage(
-                any(),
-                ON_PARTY_EMPTY,
-                any(),
-                isNull(),
-                isNull(),
-                isNull(),
-                isNull(),
-                isNull(),
-                isNull()
-            )
+        if (GlobalScope.launch { action(groupMessage, "") }.isCompleted
+            && GlobalScope.launch { action(groupMessage, null) }.isCompleted
+        ) {
+            verify(exactly = 2) {
+                bot.sendMessage(
+                    groupMessage.chat.id,
+                    ON_PARTY_EMPTY,
+                    any()
+                )
+            }
         }
     }
 
     @Test
     fun adminsPartyCall() {
-        GlobalScope.launch { action(message.copy(chat = chat.copy(type = "private")), "admins") }
+        if (GlobalScope.launch { action(privateMessage, "admins") }.isCompleted) {
+            verify(exactly = 1) {
+                bot.sendMessage(
+                    privateMessage.chat.id,
+                    ON_ADMINS_PARTY_FAIL,
+                    any()
+                )
+            }
+        }
+    }
 
-        verify(exactly = 1) {
-            bot.sendMessage(
-                any(),
-                ON_ADMINS_PARTY_FAIL,
-                any(),
-                isNull(),
-                isNull(),
-                isNull(),
-                isNull(),
-                isNull(),
-                isNull()
-            )
+    @Test
+    fun correctSinglePartyCall() {
+        val response = "@albertshady @komour"
+
+        every { partyDao.getByIdAndName(groupMessage.chat.id, "gym") } returns response
+
+        if (GlobalScope.launch { action(groupMessage, "gym") }.isCompleted) {
+            verify(exactly = 1) {
+                bot.sendMessage(
+                    groupMessage.chat.id,
+                    response,
+                    any(),
+                    isNull(),
+                    isNull(),
+                    isNull(),
+                    groupMessage.message_id
+                )
+            }
+        }
+    }
+
+    @Test
+    fun correctMultiplePartyCalls() {
+        val responseGym = "@albertshady @komour"
+        val responseSamara = "@g4nkedbymom @albertshady"
+
+        val expectedResponse = "$responseGym $responseSamara".split(' ').distinct().joinToString(" ")
+
+        every { partyDao.getByIdAndName(groupMessage.chat.id, "gym") } returns responseGym
+        every { partyDao.getByIdAndName(groupMessage.chat.id, "samara") } returns responseSamara
+
+        if (GlobalScope.launch { action(groupMessage, "gym Samara") }.isCompleted) {
+            verify(exactly = 1) {
+                bot.sendMessage(
+                    groupMessage.chat.id,
+                    expectedResponse,
+                    any(),
+                    isNull(),
+                    isNull(),
+                    isNull(),
+                    groupMessage.message_id
+                )
+            }
         }
     }
 }
---
+++ src/test/kotlin/com/github/pool_party/pull_party_bot/commands/handlers/RudeCommandTest.kt
@@ -0,0 +1,204 @@
+package com.github.pool_party.pull_party_bot.commands.handlers
+
+import com.elbekD.bot.Bot
+import com.elbekD.bot.types.Chat
+import com.elbekD.bot.types.Message
+import com.github.pool_party.pull_party_bot.commands.messages.ON_PARTY_EMPTY
+import com.github.pool_party.pull_party_bot.commands.messages.ON_RUDE_FAIL
+import com.github.pool_party.pull_party_bot.database.dao.ChatDao
+import com.natpryce.konfig.stringType
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.concurrent.CompletableFuture
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+internal class RudeCommandTest {
+    @MockK
+    private lateinit var bot: Bot
+
+    @MockK
+    private lateinit var chatDao: ChatDao
+
+    private lateinit var rude: RudeCommand
+    private lateinit var action: suspend (Message, String?) -> Unit
+
+    private val chat =
+        Chat(1, "group", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null)
+
+    private val message = Message(
+        1,
+        null,
+        null,
+        1,
+        chat,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+    )
+
+    @BeforeTest
+    fun setupMock() {
+        MockKAnnotations.init(this)
+
+        every { bot.onCommand(any(), any()) } answers { action = secondArg() }
+        every {
+            bot.sendMessage(
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                any()
+            )
+        } returns CompletableFuture()
+
+        rude = RudeCommand(chatDao)
+        rude.onMessage(bot)
+    }
+
+    @Test
+    fun wrongArgCall() {
+        if (GlobalScope.launch { action(message, "party") }.isCompleted
+            && GlobalScope.launch { action(message, "rude") }.isCompleted
+        ) {
+            verify(exactly = 2) {
+                bot.sendMessage(
+                    message.chat.id,
+                    ON_RUDE_FAIL,
+                    any()
+                )
+            }
+        }
+    }
+
+    @Test
+    fun wrongAmountOfArgsCall() {
+        if (GlobalScope.launch { action(message, "") }.isCompleted
+            && GlobalScope.launch { action(message, "first second") }.isCompleted
+        ) {
+            verify(exactly = 2) {
+                bot.sendMessage(
+                    message.chat.id,
+                    ON_RUDE_FAIL,
+                    any()
+                )
+            }
+        }
+    }
+
+    //TODO check messages for all tests.
+    @Test
+    fun correctOnSameCall() {
+        every { chatDao.getRude(message.chat.id) } returns true
+        every { chatDao.setRude(message.chat.id, true) } answers { secondArg<Boolean>() != chatDao.getRude(firstArg()) }
+
+        if (GlobalScope.launch { action(message, "on") }.isCompleted) {
+            verify(exactly = 1) {
+                bot.sendMessage(
+                    message.chat.id,
+                    any(),
+                    any()
+                )
+            }
+        }
+    }
+
+    @Test
+    fun correctOnNewCall() {
+        every { chatDao.getRude(message.chat.id) } returns false
+        every { chatDao.setRude(message.chat.id, true) } answers { secondArg<Boolean>() != chatDao.getRude(firstArg()) }
+
+        if (GlobalScope.launch { action(message, "on") }.isCompleted) {
+            verify(exactly = 1) {
+                bot.sendMessage(
+                    message.chat.id,
+                    any(),
+                    any()
+                )
+            }
+        }
+    }
+
+    @Test
+    fun correctOffSameCall() {
+        every { chatDao.getRude(message.chat.id) } returns false
+        every { chatDao.setRude(message.chat.id, false) } answers { secondArg<Boolean>() != chatDao.getRude(firstArg()) }
+
+        if (GlobalScope.launch { action(message, "off") }.isCompleted) {
+            verify(exactly = 1) {
+                bot.sendMessage(
+                    message.chat.id,
+                    any(),
+                    any()
+                )
+            }
+        }
+    }
+
+    @Test
+    fun correctOffNewCall() {
+        every { chatDao.getRude(message.chat.id) } returns true
+        every { chatDao.setRude(message.chat.id, false) } answers { secondArg<Boolean>() != chatDao.getRude(firstArg()) }
+
+        if (GlobalScope.launch { action(message, "off") }.isCompleted) {
+            verify(exactly = 1) {
+                bot.sendMessage(
+                    message.chat.id,
+                    any(),
+                    any()
+                )
+            }
+        }
+    }
+}
