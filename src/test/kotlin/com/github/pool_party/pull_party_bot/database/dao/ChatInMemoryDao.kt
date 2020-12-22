8dc0eae1df5e7d3e8fc76f5728ed0f4178389f41
Simon Naumov <daretoodefy@gmail.com>
2020-12-22 17:37:25 +0300

    :sparkles: in memory daos

---
+++ src/test/kotlin/com/github/pool_party/pull_party_bot/database/dao/ChatInMemoryDao.kt
@@ -0,0 +1,16 @@
+package com.github.pool_party.pull_party_bot.database.dao
+
+class ChatInMemoryDao(private val partyDao: PartyDao) : ChatDao {
+
+    private val chats = mutableMapOf<Long, Boolean>()
+
+    override fun getRude(chatId: Long): Boolean = chats[chatId] ?: false
+
+    override fun setRude(chatId: Long, newMode: Boolean): Boolean {
+        val previous = getRude(chatId)
+        chats[chatId] = newMode
+        return previous != newMode
+    }
+
+    override fun clear(chatId: Long) = partyDao.getAll(chatId).forEach { partyDao.delete(chatId, it.name) }
+}
