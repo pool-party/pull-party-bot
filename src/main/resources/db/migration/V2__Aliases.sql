1bea6f8f7a44106076a45398c3c2b5bfbc0ce607
Simon Naumov <daretoodefy@gmail.com>
2021-05-21 23:22:06 +0300

    :ambulance: hotfixing migration, clown moves (#126)

--- src/main/resources/db/migration/V2__Aliases.sql
+++ src/main/resources/db/migration/V2__Aliases.sql
@@ -2,17 +2,29 @@ alter table parties rename to aliases;
 
 create table parties (
     id    serial primary key,
+    first_alias_id integer not null,
     users text not null
 );
 
-insert into parties (users) select users from aliases;
+insert into parties (users, first_alias_id) select users, id from aliases;
 
 alter table aliases drop column users;
 
-alter table aliases add column party_id bigint not null;
+alter table aliases add column party_id bigint;
+
+update aliases A
+set party_id = (
+    select P.id
+    from parties P
+    where first_alias_id = A.id
+);
+
+alter table aliases alter column party_id set not null;
 
 alter table aliases add constraint fk_aliases_party_id_chat_id
     foreign key (party_id)
     references parties(id)
     on update cascade
     on delete cascade;
+
+alter table parties drop column first_alias_id;
