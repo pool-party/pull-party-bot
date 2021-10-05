c2d86e4c5302599cad29747b6016b0cce221d4df
Simon Naumov <daretoodefy@gmail.com>
2021-10-05 22:42:29 +0300

    :rotating_light: fix linter x2

--- src/test/kotlin/com/github/pool_party/pull_party_bot/commands/handlers/PullPartyBotTest.kt
+++ src/test/kotlin/com/github/pool_party/pull_party_bot/commands/handlers/PullPartyBotTest.kt
@@ -2,7 +2,6 @@ package com.github.pool_party.pull_party_bot.commands.handlers
 
 import com.github.pool_party.pull_party_bot.callbackDispatcher
 import com.github.pool_party.pull_party_bot.commands
-import com.github.pool_party.pull_party_bot.messageInteractions
 import com.github.pool_party.pull_party_bot.callback.CallbackData
 import com.github.pool_party.pull_party_bot.database.Aliases
 import com.github.pool_party.pull_party_bot.database.AliasCache
@@ -11,6 +10,7 @@ import com.github.pool_party.pull_party_bot.database.ChatCache
 import com.github.pool_party.pull_party_bot.database.Parties
 import com.github.pool_party.pull_party_bot.database.PartyAliasesCache
 import com.github.pool_party.pull_party_bot.database.PartyCache
+import com.github.pool_party.pull_party_bot.messageInteractions
 import com.github.pool_party.telegram_bot_test_utils.AbstractDatabaseBotTest
 import org.jetbrains.exposed.sql.deleteAll
 import org.jetbrains.exposed.sql.transactions.transaction
